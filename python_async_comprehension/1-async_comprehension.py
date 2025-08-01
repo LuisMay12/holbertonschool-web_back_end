d393c4b16ee058a331dbd01b0c4fc5d5c732a116
Luis May <a20214976@alumnos.uady.mx>
2025-08-01 08:12:36 -0600

    Refactor docstring in async_comprehension function for improved readability

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collect 10 random numbers from async_generator using async comprehension."""
+    """Collect 10 random numbers from async_generator
+    using async comprehension."""
     return [i async for i in async_generator()]
