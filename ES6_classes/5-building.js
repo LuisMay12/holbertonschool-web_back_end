e13baad41e8c39f8f086a6bb65134400be2908e0
Luis May <a20214976@alumnos.uady.mx>
2025-08-24 17:32:13 -0600

    add Building class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+// 5-building.js
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+
+    // Enforce abstract method override
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  // Getter
+  get sqft() {
+    return this._sqft;
+  }
+}
