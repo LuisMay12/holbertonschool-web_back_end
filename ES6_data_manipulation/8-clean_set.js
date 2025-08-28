d17b19572d1eb83d28458dc3c141edaa2efd0af0
Luis May <a20214976@alumnos.uady.mx>
2025-08-27 22:35:58 -0600

    8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+// 8-clean_set.js
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  return [...set]
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
