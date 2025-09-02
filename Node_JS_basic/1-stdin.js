f197f7d678058bdf5be6ae43ff8a17b71b6005df
Luis May <a20214976@alumnos.uady.mx>
2025-09-01 21:46:24 -0600

    fix 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 // 1-stdin.js
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+console.log("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("data", (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on("exit", () => {
+  console.log("This important software is now closing\n");
 });
